d1816e26beefa58e32a087000d4a592c5dc4e319
Christian Moyes <339463846@GoTVDSB.ca>
2024-10-03 09:10:30 -0400

    Done

--- Game1.cs
+++ Game1.cs
@@ -10,6 +10,8 @@ namespace Topic_1_Assignment___Content__Scaling__Text__and_More
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        SpriteFont speechFont;
+
         Texture2D backgroundTexture;
         Rectangle backgroundRect;
 
@@ -24,6 +26,7 @@ namespace Topic_1_Assignment___Content__Scaling__Text__and_More
 
         Texture2D zombieTexture;
         Rectangle zombieRect;
+        Rectangle zombieRect2;
 
         public Game1()
         {
@@ -42,16 +45,15 @@ namespace Topic_1_Assignment___Content__Scaling__Text__and_More
             sunflowerRect = new Rectangle(175, 240, 60, 70);
             peashooterRect = new Rectangle(245, 240, 60, 70);
             wallnutRect = new Rectangle(390, 240, 60, 70);
-            zombieRect = new Rectangle(455, 230, 77, 90);
-
-
-
+            zombieRect = new Rectangle(635, 200, 95, 120);
+            zombieRect2 = new Rectangle(585, 250, 95, 120);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            speechFont = Content.Load<SpriteFont>("Fonts/SpeechFont");
             backgroundTexture = Content.Load<Texture2D>("Images/Frontyard");
             sunflowerTexture = Content.Load<Texture2D>("Images/SunflowerSprite");
             peashooterTexture = Content.Load<Texture2D>("Images/PeashooterSprite");
@@ -77,8 +79,12 @@ namespace Topic_1_Assignment___Content__Scaling__Text__and_More
             _spriteBatch.Draw(backgroundTexture, backgroundRect, Color.White);
             _spriteBatch.Draw(sunflowerTexture, sunflowerRect, Color.White);
             _spriteBatch.Draw(peashooterTexture, peashooterRect, Color.White);
-            _spriteBatch.Draw(wallnutTexture, wallnutRect, Color.White);
+            _spriteBatch.Draw(wallnutTexture, wallnutRect, null, Color.White, 0f, new Vector2(0, 0), SpriteEffects.FlipVertically, 1f);
             _spriteBatch.Draw(zombieTexture, zombieRect, Color.White);
+            _spriteBatch.Draw(zombieTexture, zombieRect2, null, Color.White * 0.5f, 1.57f, new Vector2(0,0), SpriteEffects.None, 1f);
+            _spriteBatch.DrawString(speechFont, "Why is he on his head?", new Vector2(150, 210), Color.Black);
+            _spriteBatch.DrawString(speechFont, "Gymnastics.", new Vector2(240, 325), Color.Black);
+            _spriteBatch.DrawString(speechFont, "(Killed by the awesome power of the headstanding wallnut).", new Vector2(350, 330), Color.Black);
             _spriteBatch.End();
             base.Draw(gameTime);
         }
